b8a4732c2f726f64f82f8f8effd06cb2456564c3
Jefferson Scher <jscher2000@gmail.com>
2020-05-10 12:15:02 -0700

    version 0.5

---
+++ options.js
@@ -0,0 +1,65 @@
+/* 
+  Copyright 2020. Jefferson "jscher2000" Scher. License: MPL-2.0.
+  version 0.5 - initial concept
+*/
+
+/*** Initialize Page ***/
+
+// Default starting values
+var oPrefs = {
+	opennewtab: false,			// direct site to a new tab by default
+	newtabactive: true,			// make the new tab active
+	newwinprivate: false,		// whether new windows are private by default
+	listmax: 15,				// max sites to show on list
+	showBlocked: false,			// include sites blocked on the new tab page?
+	showPinned: false,			// include sites pinned on the new tab page?
+	searchShortcuts: false		// include search shortcuts from the new tab page?
+}
+
+// Update oPrefs from storage and update form values
+browser.storage.local.get("prefs").then((results) => {
+	if (results.prefs != undefined){
+		if (JSON.stringify(results.prefs) != '{}'){
+			var arrSavedPrefs = Object.keys(results.prefs)
+			for (var j=0; j<arrSavedPrefs.length; j++){
+				oPrefs[arrSavedPrefs[j]] = results.prefs[arrSavedPrefs[j]];
+			}
+		}
+	}
+	// Populate checkboxes
+	var chks = document.querySelectorAll('input[type="checkbox"]');
+	for (var i=0; i<chks.length; i++){
+		if (oPrefs[chks[i].name] == true) chks[i].checked = true;
+		else chks[i].checked = false;
+	}
+	// Set the numeric input
+	var numsites = document.querySelector('input[name="numsites"]');
+	numsites.value = oPrefs.listmax;
+}).catch((err) => {
+	document.getElementById('oops').textContent = 'Error retrieving "prefs" from storage or setting up form: ' + err.message;
+});
+
+/*** Handle User Actions ***/
+
+// Update storage
+function updatePrefs(evt){
+	// Checkboxes
+	var chks = document.querySelectorAll('.chk input[type="checkbox"]');
+	for (var i=0; i<chks.length; i++){
+		oPrefs[chks[i].name] = chks[i].checked;
+	}
+	// Update listmax
+	var numsites = document.querySelector('input[name="numsites"]').value;
+	if (numsites >= 1 && numsites <= 100){
+		oPrefs.listmax = numsites;
+	}
+	// Update storage
+	browser.storage.local.set(
+		{prefs: oPrefs}
+	).catch((err) => {
+		document.getElementById('oops').textContent = 'Error on browser.storage.local.set(): ' + err.message;
+	});
+}
+
+// Attach event handlers 
+document.getElementById('btnsave').addEventListener('click', updatePrefs, false);
---
+++ popup.js
@@ -0,0 +1,124 @@
+/* 
+  Copyright 2020. Jefferson "jscher2000" Scher. License: MPL-2.0.
+  version 0.5 - initial concept
+*/
+
+/*** Lay out the list (later: get stored preferences first) ***/
+
+// Default starting values
+var oPrefs = {
+	opennewtab: false,			// direct site to a new tab by default
+	newtabactive: true,			// make the new tab active
+	newwinprivate: false,		// whether new windows are private by default
+	listmax: 20,				// max sites to show on list
+	showBlocked: false,			// include sites blocked on the new tab page?
+	showPinned: false,			// include sites pinned on the new tab page?
+	searchShortcuts: false		// include search shortcuts from the new tab page?
+}
+
+// Update oPrefs from storage
+browser.storage.local.get("prefs").then((results) => {
+	if (results.prefs != undefined){
+		if (JSON.stringify(results.prefs) != '{}'){
+			var arrSavedPrefs = Object.keys(results.prefs)
+			for (var j=0; j<arrSavedPrefs.length; j++){
+				oPrefs[arrSavedPrefs[j]] = results.prefs[arrSavedPrefs[j]];
+			}
+		}
+	}
+	var gettingTopSites = browser.topSites.get({
+		newtab: false,
+		onePerDomain: false,
+		includeFavicon: true,
+		limit: parseInt(oPrefs.listmax),
+		includeBlocked: oPrefs.showBlocked,
+		includePinned: oPrefs.showPinned,
+		includeSearchShortcuts: oPrefs.searchShortcuts
+	});
+	gettingTopSites.then((arrSites) => {
+		var list = document.getElementById('frecentlist');
+		var newLI = document.getElementById('newLI');
+		for (var i=0; i<arrSites.length; i++){
+			var clone = document.importNode(newLI.content, true);
+			// Populate the template
+			var elTemp = clone.querySelector('li');
+			elTemp.setAttribute('title', arrSites[i].title + ' - ' + arrSites[i].url);
+			elTemp = clone.querySelector('img');
+			elTemp.setAttribute('src', fixPath(arrSites[i]));
+			elTemp = clone.querySelectorAll('span');
+			elTemp[0].appendChild(document.createTextNode(arrSites[i].title));
+			elTemp[2].appendChild(document.createTextNode(arrSites[i].url));
+			// Add the item to the list
+			list.appendChild(clone);
+		}
+	});
+}).catch((err) => {
+	document.getElementById('oops').textContent = 'Error retrieving "prefs" from storage or building list: ' + err.message;
+});
+
+function fixPath(site){
+	if (site.favicon){
+		return site.favicon;
+	} else {
+		if (site.url.indexOf('http://') == 0 || site.url.indexOf('https://') == 0){
+			return 'icons/defaultFavicon.svg';
+		}
+		if (site.url.indexOf('file:///') == 0){
+			return 'icons/folder-16.svg';
+		}
+		return 'icons/square.svg';
+	}
+}
+
+/*** Handle User Interaction ***/
+
+function openFrecent(evt){
+	console.log(evt);
+	// Get the li the user clicked
+	var tgt = evt.target;
+	if (tgt.nodeName != "LI"){
+		tgt = tgt.closest('li');
+		if (!tgt){
+			document.getElementById('oops').textContent = 'Script is confused about what you clicked. Try again?';
+			return;
+		}
+	}
+	// Get the URL from the second span in the li
+	var siteUrl = tgt.querySelectorAll('span')[2].textContent;
+	// Where to open
+	var CtrlCommand = (browser.runtime.PlatformOs == 'mac') ? evt.metaKey : evt.ctrlKey;
+	if (evt.shiftKey){ // Open new window
+		var priv = false;
+		if (oPrefs.newwinprivate == true || (evt.altKey && oPrefs.newwinprivate == false)) priv = true;
+		browser.windows.create({
+			incognito: priv,
+			url: siteUrl
+		}).catch((err) => {
+			document.getElementById('oops').textContent = 'Error opening new window: ' + err.message;
+		});
+	} else if ((oPrefs.opennewtab === true && !CtrlCommand) || (oPrefs.opennewtab === false && CtrlCommand)){ // Open new tab
+		browser.tabs.create({
+			active: oPrefs.newtabactive,
+			url: siteUrl
+		}).then(() => {
+			if (oPrefs.newtabactive == true) self.close(); // Close the popup if we changed tabs
+		}).catch((err) => {
+			document.getElementById('oops').textContent = 'Error opening new tab: ' + err.message;
+		});
+	} else { // Navigate current tab
+		browser.tabs.update({
+			url: siteUrl
+		}).then(() => {
+			self.close(); // Close the popup
+		}).catch((err) => {
+			document.getElementById('oops').textContent = 'Error navigating this tab: ' + err.message;
+		});
+	}
+}
+
+// Attach event handlers 
+document.getElementById('frecentlist').addEventListener('click', openFrecent, false);
+document.getElementById('options').addEventListener('click', function(){
+	browser.runtime.openOptionsPage();
+	self.close();
+}, false);
